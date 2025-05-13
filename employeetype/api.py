a1caa94c30e5a24725ae43965a07ba3873221842
Subesh Pandey <pandeypaul137@gmail.com>
2025-05-13 11:26:31 -0500

    Resolving the Error

--- employeetype/api.py
+++ employeetype/api.py
@@ -1,4 +1,4 @@
 from .views import EmployeeTypeViewSet
 
 def register(router):
-    router.register(r'employeetype', EmployeeTypeViewSet, base_name = 'employeetype')
+    router.register(r'employeetype', EmployeeTypeViewSet, basename='employeetype')
