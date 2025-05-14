461967aab44bbad3f0d72b0e56ac819e5c9d4217
Subesh Pandey <pandeypaul137@gmail.com>
2025-05-14 15:13:55 -0500

    Creating the app Biweekly CronJob

---
+++ biweeklycron/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+class biweeklycron(models.Model):
+    run_date = models.DateField(unique=True)
+    is_active = models.BooleanField(default=True)
+    tag = models.CharField(max_length=100)
+    class Meta:
+        verbose_name = "Biweekly Cron"
+        verbose_name_plural = "Biweekly Crons"
