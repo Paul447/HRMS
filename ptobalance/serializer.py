f43ba27a197a7fa3cc65213538c2ccd2e6849624
Subesh Pandey <pandeypaul137@gmail.com>
2025-05-21 12:27:09 -0500

    rephrasing the serializer for specific details needed to the frontend

--- ptobalance/serializer.py
+++ ptobalance/serializer.py
@@ -29,69 +29,12 @@ class YearOfExperienceSerializer(serializers.ModelSerializer):
         fields = ['years_of_experience']
 
 class PTOBalanaceSerializer(serializers.ModelSerializer):
-    url = serializers.HyperlinkedIdentityField(view_name = 'ptobalance-detail')
     employee_type = EmployeeTypeSerializer(read_only=True)
     pay_frequency = PayFrequencySerializer(read_only=True)
     accrual_rate = AccuralRateSerializer(read_only = True)
     user = UserSerializer(read_only = True)
     year_of_experience = YearOfExperienceSerializer(read_only = True)
-    employee_type_id = serializers.PrimaryKeyRelatedField(
-        queryset=EmployeeType.objects.all(),
-        source='employee_type',
-        write_only=True
-    )
-    pay_frequency_id = serializers.PrimaryKeyRelatedField(
-        queryset=Pay_Frequency.objects.all(),
-        source='pay_frequency',
-        write_only=True
-    )
-    user_id = serializers.PrimaryKeyRelatedField(
-        queryset = User.objects.all(),
-        source = 'user',
-        write_only=True
-    )
     class Meta:
         model = PTOBalance
-        fields ='__all__'
-    def create(self, validated_data):
-        user = validated_data['user']
-        employeetype = validated_data['employee_type']
-        payfrequency = validated_data['pay_frequency']
-
-        year_exp = YearOfExperience.objects.filter(user=user).first()
-        if year_exp:
-            validated_data['year_of_experience'] = year_exp
-            yoe = year_exp.years_of_experience
-
-            if yoe < 1:
-                x = 1
-            elif yoe < 2:
-                x = 2
-            elif yoe < 3:
-                x = 3
-            elif yoe < 4:
-                x = 4
-            elif yoe < 5:
-                x = 5
-            elif yoe < 6:
-                x = 6
-            elif yoe < 7:
-                x = 7
-            elif yoe < 8:
-                x = 8
-            elif yoe < 9:
-                x = 9
-            elif yoe < 10:
-                x = 10
-            else:
-                x = 11
-
-            accrualrate = AccrualRates.objects.filter(
-                employee_type=employeetype,
-                pay_frequency=payfrequency,
-                year_of_experience=x
-            ).first()
-
-            validated_data['accrual_rate'] = accrualrate
-
-        return super().create(validated_data)
+        fields = ['employee_type', 'pay_frequency', 'accrual_rate', 'user', 'year_of_experience', 'pto_balance']
+    
