78886869958b7f026e01c15a82c8af3a21537554
Subesh Pandey <pandeypaul137@gmail.com>
2025-09-29 13:20:33 -0500

    Removing the YOE application

--- yearofexperience/apps.py
+++
@@ -1,9 +0,0 @@
-# from django.apps import AppConfig
-
-
-# class YearofexperienceConfig(AppConfig):
-#     default_auto_field = "django.db.models.BigAutoField"
-#     name = "yearofexperience"
-
-#     def ready(self):
-#         import yearofexperience.signals
